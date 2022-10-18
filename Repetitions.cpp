0b5ba2e304206b1995906cd2a7a6e8b2fb939a85
Priyanshu <princepriyanshu.vaishnav@gmail.com>
2022-10-18 22:45:02 +0530

    CSES DAY 1

---
+++ Repetitions.cpp
@@ -0,0 +1,79 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize('Ofast')
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define nline '\n'
+#define mm (9*1e18)
+#define mp make_pair
+#define pb push_back
+#define int long long
+#define mn ((-9)*1e18)
+#define lb lower_bound
+#define ub upper_bound
+#define MOD 1000000007
+using namespace chrono;
+#define bs binary_search
+#define size(x) (x).size()
+#define double long double
+#define PI 3.141592653589793238462
+#define all(x) (x).begin(), (x).end()
+#define set_bits __builtin_popcountll
+#define fora(i,a,b)  for(int i=a;i<b;i++)
+#define rfora(i,a,b) for(int i=b-1;i>=a;i--)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+// #ifndef ONLINE_JUDGE
+// #include <bits/debug.h>
+// #else
+// #define debug(x)
+// #endif
+/*----------------------------------------------------------------------------------------*/
+
+
+void rogenx() {
+//Write your code here
+  string s;
+  cin>>s;
+  int n=size(s);
+  int maxi=0;
+
+  int i=0;
+  while(i<n){
+      char fd=s[i];
+      int cnt=0;
+      while(s[i]==fd)
+      {
+        i++;
+        cnt++;
+      }
+      maxi=max(maxi,cnt);
+
+  }
+  cout<<maxi;
+}
+
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+freopen("error.txt", "w", stderr);
+#endif
+fastio();
+auto start1 = high_resolution_clock::now();
+// cout << fixed << setprecision(10);
+int _ = 1, gcode = 1;
+//cin >> _;
+while (_--) {
+//cout << "Case #" << gcode++ << ": ";
+rogenx();
+}
+auto stop1 = high_resolution_clock::now();
+auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+cerr << "Time: " << duration . count() / 1000 << "ms" << nline;
+#endif
+return 0;
+}
